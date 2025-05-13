8761f4ad085c51f94a6f46473496f1b362311559
Alumnos MCSD Mañana <Alumnos MCSD Maana@T06W16>
2025-05-13 11:10:41 +0200

    Agregar archivos de proyecto.

---
+++ ApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,36 @@
+﻿using ApiPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonajesAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private PersonajesRepository _repository;
+
+        public PersonajesController(PersonajesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPersonajes()
+        {
+            var personajes = await _repository.GetPersonajesAsync();
+            return Ok(personajes);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePersonaje([FromBody] Models.Personaje personaje)
+        {
+            if (personaje == null)
+            {
+                return BadRequest("El personaje no puede ser nulo.");
+            }
+            await _repository.CreatePersonajeAsync(personaje);
+            return Ok();
+        }
+    }
+}
---
+++ ApiPersonajesAWS/Repositories/PersonajesRepository.cs
@@ -0,0 +1,35 @@
+﻿using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Repositories
+{
+    public class PersonajesRepository
+    {
+
+        private PersonajesContext _context;
+
+        public PersonajesRepository(PersonajesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            return await _context.Personajes.ToListAsync();
+        }
+
+        public async Task<int> GetMaxIdPersonajeAsync()
+        {
+            return await _context.Personajes.MaxAsync(p => p.IdPersonaje);
+        }
+
+        public async Task CreatePersonajeAsync(Personaje personaje)
+        {
+            var maxId = await GetMaxIdPersonajeAsync();
+            personaje.IdPersonaje = maxId + 1;
+            await _context.Personajes.AddAsync(personaje);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
